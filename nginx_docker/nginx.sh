34fc2bcfda425f85aa54a5a59484bdb434f23daf
dengwenqi123 <wenqi@megacredit.cc>
2019-09-22 11:38:20 +0800

    nginx

--- nginx_docker/nginx.sh
+++ nginx_docker/nginx.sh
@@ -1,1 +1,1 @@
-docker run -p 80:80 --name nginx -v $PWD/www:/www -v $PWD/conf/nginx.conf:/etc/nginx/nginx.conf -v $PWD/logs:/wwwlogs -d nginx
+docker run -p443:443 -p 80:80 --name nginx -v $PWD/www:/www -v $PWD/cert:/etc/nginx/cert -v $PWD/conf/nginx.conf:/etc/nginx/nginx.conf -v $PWD/logs:/wwwlogs -d nginx
